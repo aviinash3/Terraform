44043f203c9524ef184204472f1eca2764229258
aviinash3 <aviikr3@gmail.com>
2021-03-12 15:51:04 +0530

    first ec2 instance

---
+++ aws/first_instance.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = ""
+  secret_key = ""
+
+}
+resource "aws_instance" "AWSEC2Instance" {
+  ami = "ami-0a9d27a9f4f5c0efc"
+  instance_type   = "t2.micro"
+#  security_groups = ["default"]
+#  key_name        = "ubuntu-1"
+  tags = {
+    Name = "Redhat linux"
+  }
+
+}
